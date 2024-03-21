58041613f06bfaf6d4d5f1d39f286b905e2cc889
Olusegun Ekundayo Adebayo <segun.adebayo65@yahoo.com>
2024-03-21 22:51:19 +0100

    b

--- src/cnnClassifier_benign_vs_malignant/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier_benign_vs_malignant/pipeline/stage_02_prepare_base_model.py
@@ -24,8 +24,8 @@ if __name__ == '__main__':
     try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = PrepareBaseModelTrainingPipeline()
-        obj.main()
+        base_model_training = PrepareBaseModelTrainingPipeline()
+        base_model_training.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
